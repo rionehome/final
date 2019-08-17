acc7fbb2cefd4aac86cb4e41f66cec047d0553ee
MiglyA <zq6295migly@gmail.com>
2019-08-17 19:58:01 +0900

    [update]launch追加

--- scripts/final.py
+++ scripts/final.py
@@ -9,13 +9,13 @@ import difflib  # 類似度を計算するライブラリ
 
 class RestaurantFinal:
     def __init__(self):
-        rospy.init_node("restaurant_finish", anonymous=True)
-
+        rospy.init_node("final_finish", anonymous=True)
+        
         self.call_ducker_pub = rospy.Publisher("/call_ducker/control", String, queue_size=10)
-
+        
         rospy.Subscriber("/order_input", String, self.order_text_callback)
         rospy.Subscriber("/call_ducker/finish", Bool, self.call_ducker_callback)
-
+    
     def order_text_callback(self, data):
         # type:(String) -> None
         """
@@ -24,9 +24,9 @@ class RestaurantFinal:
         :return: なし
         """
         self.print_function("order_text_callback")
-
+        
         web_text = data.data.lower()
-
+        
         order_sentence_list = ["i want beans", "i want biscuits", "i want coca cola", "i want cookies",
                                "i want grape juice"]
         # 類似度計算
@@ -37,20 +37,20 @@ class RestaurantFinal:
             if score < s:
                 score = s
                 order_sentence = x
-
+        
         order = " ".join(order_sentence.split()[2:])  # 商品名
-
+        
         # キッチンに着いた
         self.speak("Order is {}.".format(order))
         self.speak("Please give me items.")
-
+        
         time.sleep(5)
-
+        
         self.speak("I will deliver {}. Please raise your hand.".format(order))
-
+        
         # call_duckerにメッセージを送信
         self.call_ducker_pub.publish("start")
-
+    
     def call_ducker_callback(self, msg):
         """
         call_duckerのcallback関数
@@ -65,7 +65,7 @@ class RestaurantFinal:
         else:
             self.speak("Sorry, Please carry it in front of the customer.")
             print "お客さんの前に運んでください。"
-
+    
     @staticmethod
     def speak(text):
         # type: (str) -> None
@@ -77,7 +77,7 @@ class RestaurantFinal:
         """
         rospy.wait_for_service("/sound_system/speak")
         rospy.ServiceProxy("/sound_system/speak", StringService)(text)
-
+    
     @staticmethod
     def print_function(name):
         # type: (str) -> None
